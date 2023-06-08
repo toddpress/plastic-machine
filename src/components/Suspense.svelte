a0172524ab154ed8bf0588e077da0434cd7f2c17
Todd Pressley <todd@echovate.com>
2023-06-08 00:57:53 -0400

    add WIP suspense component to improve UX for async loading state

---
+++ src/components/Suspense.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  export let component;
+  export let fallback;
+</script>
+
+{#if component === null}
+  {#if fallback}
+    <slot name="fallback" />
+  {:else}
+    <div
+      class="radial-progress"
+      style="--value: 60; --size: 3.5rem;"
+    >
+    </div>
+  {/if}
+{:else}
+  <svelte:component this={component} />
+{/if}
+
+
