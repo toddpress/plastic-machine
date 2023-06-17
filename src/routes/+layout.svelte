211569512392c38e239901a124e8493651c04ad6
Todd Pressley <todd@echovate.com>
2023-06-17 17:29:47 -0400

    add home link to navbar brand

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,7 +16,9 @@
 						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
 					</label>
 				</div>
-				<div class="flex-1 px-2 mx-2 text-xl">Plastic Machine</div>
+				<div class="flex-1 px-2 mx-2 text-xl">
+					<a href="/">Plastic Machine</a>
+				</div>
 			</div>
 			<div class="navbar-end">
 				<div class="hidden lg:flex justify-end">
